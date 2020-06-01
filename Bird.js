8ef00296408e1d6e9c3c2885040b58b82ce0a218
Keishaa <keishaavarma@gmail.com>
2020-06-01 19:54:29 +0530

    smoke

--- Bird.js
+++ Bird.js
@@ -2,12 +2,22 @@ class Bird extends BaseClass {
   constructor(x,y){
     super(x,y,50,50);
     this.image = loadImage("sprites/bird.png");
+    this.trajectory= [];
+    this.image1= loadImage("sprites/smoke.png");
   }
 
   display() {
-    //this.body.position.x = mouseX;
-    //this.body.position.y = mouseY;
+   super.display();
+   console.log(this.body.speed);
+   if(this.body.speed>10&& this.body.position.x>250){
+   var pos= [this.body.position.x, this.body.position.y];
+   this.trajectory.push(pos);
+   }
+   max= this.trajectory.length-1;
+   for (var i=0; i<=max; i++){
+     image(this.image1, this.trajectory[i][0], this.trajectory[i][1] );
+   }
 
-    super.display();
+  
   }
 }
